e4840fce1d3ab17d90c30ed94bdfae8ce0bfecef
akagisho <akagisho@gmail.com>
2013-06-10 20:22:59 +0900

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fusen::Application.config.secret_token = 'ff0f299b212955f1e72b8a7a2605db9adacbee433b4b375d46cd98c77e0fde24871418fc1a9cf6dc537957c21ec111ce9dd6a32e29f52b5fda0231a244311ba8'
